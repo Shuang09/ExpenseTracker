030ae87219f418ffa398951dde9707ca0026abca
Susana <yshuhang09@gmail.com>
2021-01-09 10:13:08 -0500

    Add Dao

---
+++ src/page/SpendPage.java
@@ -0,0 +1,35 @@
+/* shuhangyan created on 2021-01-09 inside the package - page */
+package page;
+
+public class SpendPage {
+    public String monthSpend;
+    public String todaySpend;
+    public String avgSpendPerDay;
+    public String monthAvailable;
+    public String dayAvgAvailable;
+    public String monthLeftDay;
+    public int usagePercentage;
+    public boolean isOverSpend = false;
+
+    public SpendPage(int monthSpend, int todaySpend, int avgSpendPerDay, int monthAvailable, int dayAvgAvailable,
+                     int monthLeftDay, int usagePercentage) {
+        this.monthSpend = "£¤" + monthSpend;
+        this.todaySpend = "£¤" + todaySpend;
+        this.avgSpendPerDay = "£¤" + avgSpendPerDay;
+        if (monthAvailable < 0)
+            isOverSpend = true;
+
+        if (!isOverSpend) {
+            this.monthAvailable = "£¤" + monthAvailable;
+            this.dayAvgAvailable = "£¤" + dayAvgAvailable;
+        } else {
+            this.monthAvailable = "³¬Ö§" + (0 - monthAvailable);
+            this.dayAvgAvailable = "£¤0";
+        }
+
+        this.monthLeftDay = monthLeftDay + "Ìì";
+        this.usagePercentage = usagePercentage;
+    }
+
+
+}
